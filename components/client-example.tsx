d7026cbcd2d5ee38c80c5a3999fe22e537332048
kushidam <131370475+kushidam@users.noreply.github.com>
2024-04-21 02:14:35 +0900

    Add セッション情報View

--- components/client-example.tsx
+++ components/client-example.tsx
@@ -31,6 +31,14 @@ export default function ClientExample() {
         </CustomLink>
         コンポーネントが必要です。
       </p>
+      <div className="flex flex-col rounded-md bg-neutral-100">
+        <div className="p-4 font-bold rounded-t-md bg-neutral-200">
+          Current Session
+        </div>
+        <pre className="py-6 px-4 whitespace-pre-wrap break-all">
+          {JSON.stringify(session, null, 2)}
+        </pre>
+      </div>
     </div>
   );
 }
